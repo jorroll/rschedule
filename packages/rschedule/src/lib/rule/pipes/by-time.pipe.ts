bb1d5a2a19ac71e97c8c0744ccb4a1568822166b
John Carroll <john.carroll.p@gmail.com>
2019-08-19 16:43:14 -0700

    docs: add comments

--- packages/rschedule/src/lib/rule/pipes/by-time.pipe.ts
+++ packages/rschedule/src/lib/rule/pipes/by-time.pipe.ts
@@ -18,19 +18,25 @@ export abstract class ByTimePipe<T> extends PipeRule<T> {
 
       let { date } = args;
 
+      // e.g. const currentTime = date.get('hour');
       const currentTime = date.get(granularity as any);
 
+      // e.g. for (const time of this.options.byHourOfDay) {
       for (const time of (this.options as any)[optionName]) {
         if (currentTime > time) continue;
 
         if (currentTime === time) return this.nextPipe.run({ date });
 
+        // e.g. return this.nextValidDate(args, date.granularity('day').set('hour', time));
         return this.nextValidDate(args, date.granularity(baseGranularity).set(granularity, time));
       }
 
       date = date
+        // e.g. .granularity('day')
         .granularity(baseGranularity)
+        // e.g. .add(1, 'day')
         .add(1, baseGranularity)
+        // e.g. .set('hour', this.options.byHourOfDay[0]);
         .set(granularity, (this.options as any)[optionName][0]);
 
       return this.nextValidDate(args, date);
--- packages/rschedule/src/lib/rule/pipes/rev-by-time.pipe.ts
+++ packages/rschedule/src/lib/rule/pipes/rev-by-time.pipe.ts
@@ -18,13 +18,16 @@ export abstract class RevByTimePipe<T> extends PipeRule<T> {
 
       let { date } = args;
 
+      // e.g. const currentTime = date.get('hour');
       const currentTime = date.get(granularity as any);
 
+      // e.g. for (const time of this.options.byHourOfDay) {
       for (const time of (this.options as any)[optionName]) {
         if (currentTime < time) continue;
 
         if (currentTime === time) return this.nextPipe.run({ date });
 
+        // e.g. return this.nextValidDate(args, date.endGranularity('day').set('hour', time));
         return this.nextValidDate(
           args,
           date.endGranularity(baseGranularity).set(granularity, time),
@@ -32,8 +35,11 @@ export abstract class RevByTimePipe<T> extends PipeRule<T> {
       }
 
       date = date
+        // e.g. .endGranularity('day')
         .endGranularity(baseGranularity)
+        // e.g. .subtract(1, 'day')
         .subtract(1, baseGranularity)
+        // e.g. .set('hour', this.options.byHourOfDay[0]);
         .set(granularity, (this.options as any)[optionName][0]);
 
       return this.nextValidDate(args, date);
