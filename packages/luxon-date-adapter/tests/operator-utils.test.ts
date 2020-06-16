729cff716173aa0d8774ab06901f0d5b93f7084c
John Carroll <john.carroll.p@gmail.com>
2020-06-16 01:08:48 -0700

    add: missing operator-utils tests

---
+++ packages/luxon-date-adapter/tests/operator-utils.test.ts
@@ -0,0 +1,4 @@
+import '@rschedule/luxon-date-adapter/setup';
+import tests from '../../core/src/generators/operators/_util-test-setup';
+
+tests();
---
+++ packages/standard-date-adapter/tests/operator-utils.test.ts
@@ -0,0 +1,4 @@
+import '@rschedule/standard-date-adapter/setup';
+import tests from '../../core/src/generators/operators/_util-test-setup';
+
+tests();
